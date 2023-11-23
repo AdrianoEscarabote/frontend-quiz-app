3918e315c33522af62dd3ad08ee3106e084271c4
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-23 13:05:22 -0300

    feat(components): adding CategoryTag component and updating header

---
+++ src/components/CategoryTag/CategoryTag.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react";
+import CategoryTag from ".";
+
+export default {
+  title: "components/CategoryTag",
+  component: CategoryTag,
+} as Meta;
+
+export const Primary: StoryObj = {};
