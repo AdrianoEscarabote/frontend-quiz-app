ea5cad0c10fd55e68d34bcff49e2b48e14876465
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-23 13:06:49 -0300

    feat(app): adding page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import HomeComponent from "@/components/HomeComponent";
+
+export const metadata = {
+  title: "Frontend quiz app",
+};
+
+export default function Home() {
+  return <HomeComponent />;
+}
