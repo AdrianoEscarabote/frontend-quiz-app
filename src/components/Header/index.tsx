5085f1c6d251a983bd16bbd21b03d0b54b1d805a
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-24 16:07:27 -0300

    style(components): improving responsiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import ToggleThemeButton from "../ToggleThemeButton";
 import { RootState } from "@/redux/quizData/quizDataTypes";
 import { HeaderProps } from "./Header";
 import CategoryTag from "../CategoryTag";
+import style from "./style.module.css";
 
 const Header = ({ showCategory }: HeaderProps) => {
   const { title } = useSelector((state: RootState) => {
@@ -11,8 +12,10 @@ const Header = ({ showCategory }: HeaderProps) => {
   });
 
   return (
-    <header className="w-full max-w-[1440px] flex justify-between items-center">
-      <CategoryTag showCategory={showCategory} title={title} />
+    <header
+      className={`${style.header} w-full max-w-[1440px] flex justify-between items-center`}
+    >
+      <CategoryTag showCategory={showCategory} title={title as any} />
 
       <div className="flex justify-end w-full">
         <ToggleThemeButton />
