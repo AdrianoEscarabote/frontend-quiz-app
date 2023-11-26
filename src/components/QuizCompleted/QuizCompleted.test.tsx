47155d4a48c731e9121a18977ac5c4b30ba18fb4
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-11-25 23:22:44 -0300

    feat(components): adding quiz completed component

---
+++ src/components/QuizCompleted/QuizCompleted.test.tsx
@@ -0,0 +1,30 @@
+import { render } from "@testing-library/react";
+import QuizCompleted from ".";
+import getMockState from "@/utils/getMockState";
+import configureMockStore from "redux-mock-store";
+import { Provider } from "react-redux";
+
+const mockStore = configureMockStore();
+
+describe("QuizCompleted", () => {
+  let store: any;
+
+  beforeEach(() => {
+    const mockState = getMockState();
+    const state = mockStore(mockState);
+
+    store = state;
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should render correctly", () => {
+    render(
+      <Provider store={store}>
+        <QuizCompleted handleShowHomeComponent={() => {}} />
+      </Provider>
+    );
+  });
+});
